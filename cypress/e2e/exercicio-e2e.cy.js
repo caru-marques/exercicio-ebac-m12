9abd2274ca2395deb281fb3172212a7815953744
Carolina Marques <carolina.marques@ifood.com.br>
2024-09-05 17:36:16 -0300

    Adicionar lógica de exercício proposto

--- cypress/e2e/exercicio-e2e.cy.js
+++ cypress/e2e/exercicio-e2e.cy.js
@@ -1,4 +1,8 @@
 /// <reference types="cypress" />
+import page from "../support/page_objects/nome-funcionliada.page";
+const perfil = require('../fixtures/perfil.json')
+const form = require('../fixtures/checkout_form.json')
+
 
 context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
   /*  Como cliente 
@@ -10,13 +14,38 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
       E validando minha compra ao final */
 
   beforeEach(() => {
-      cy.visit('/')
+    page.visitUrl()
   });
 
   it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
-      //TODO: Coloque todo o fluxo de teste aqui, considerando as boas práticas e otimizações
-      
+    page.login(perfil.usuario, perfil.senha)
+    const usuarioNome = perfil.usuario.split('@')[0];
+    cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', `Olá, ${usuarioNome} (não é ${usuarioNome}? Sair)`)
+
+    page.searchProduct('Augusta Pullover Jacket')
+    cy.get('.product_title').should('contain', 'Augusta Pullover Jacket')
+    page.addProductToCart('S', 'Orange', 1)
+    cy.get('.woocommerce-message').should('contain', '“Augusta Pullover Jacket” foi adicionado no seu carrinho.')
+
+    page.searchProduct('Autumn Pullie')
+    cy.get('.product_title').should('contain', 'Autumn Pullie')
+    page.addProductToCart('M', 'Green', 1)
+    cy.get('.woocommerce-message').should('contain', '“Autumn Pullie” foi adicionado no seu carrinho.')
+
+    page.searchProduct('Sinbad Fitness Tank')
+    cy.get('.product_title').should('contain', 'Sinbad Fitness Tank')
+    page.addProductToCart('L', 'Blue', 1)
+    cy.get('.woocommerce-message').should('contain', '“Sinbad Fitness Tank” foi adicionado no seu carrinho.')
+
+    page.searchProduct('Geo Insulated Jogging Pant')
+    cy.get('.product_title').should('contain', 'Geo Insulated Jogging Pant')
+    page.addProductToCart('36', 'Red', 1)
+    cy.get('.woocommerce-message').should('contain', '“Geo Insulated Jogging Pant” foi adicionado no seu carrinho.')
+
+    page.goToCart()
+
+    page.goToCheckout()
+
+    page.fillFormCheckout(form.firstName, form.lastName, form.country, form.street, form.complement, form.city, form.state, form.zipCode, form.phone, form.email)
   });
-
-
 })
--- cypress/support/page_objects/nome-funcionliada.page.js
+++ cypress/support/page_objects/nome-funcionliada.page.js
@@ -1,7 +1,52 @@
 class NomeClasse {
 
-    nomeMetodo(parametros ){
-        //ações do método
+    visitUrl() {
+        cy.visit('minha-conta')
+    }
+
+    login(usuario, senha) {
+        cy.get('#username').type(usuario)
+        cy.get('#password').type(senha)
+        cy.get('.woocommerce-form > .button').click()
+    }
+
+    searchProduct(produto) {
+        cy.get('[name="s"]').eq(1).type(produto)
+        cy.get('.button-search').eq(1).click()
+    }
+
+    addProductToCart(tamanho, cor, quantidade) {
+        cy.get(`.button-variable-item-${tamanho}`).click()
+        cy.get(`.button-variable-item-${cor}`).click()
+        cy.get('.input-text').clear().type(quantidade)
+        cy.get('.single_add_to_cart_button').click()
+    }
+
+    goToCart() {
+        cy.get('.woocommerce-message > .button').click()
+    }
+
+    goToCheckout() {
+        cy.get('.checkout-button').click()
+    }
+
+    fillFormCheckout(firstName, lastName, country, street, complement, city, state, zipCode, phone, email) {
+        cy.get('#billing_first_name').clear().type(firstName)
+        cy.get('#billing_last_name').clear().type(lastName)
+        cy.get('#billing_country_field > .woocommerce-input-wrapper > .select2 > .selection > .select2-selection > .select2-selection__arrow > b').click()
+        cy.get('.select2-search__field').type(country)
+        cy.get('#billing_address_1').type(street)
+        cy.get('#billing_address_2').clear().type(complement)
+        cy.get('#billing_city').clear().type(city)
+        cy.get('#billing_state_field > .woocommerce-input-wrapper > .select2 > .selection > .select2-selection > .select2-selection__arrow').click()
+        cy.get('.select2-search__field').type(state)
+        cy.get('#billing_postcode').clear().type(zipCode)
+        cy.get('#billing_phone').clear().type(phone)
+        cy.get('#billing_email').clear().type(email)
+        cy.get('#terms').click()
+        cy.get('#place_order').click()
+
+        
     }
 
 }
